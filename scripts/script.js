529ed868ed2ef3798c49a651e3412ed1ca9e59f7
naiem <naiem4288@gamil.com>
2020-11-01 03:39:26 +0600

    food template

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0)
+});
+
+
+// toggle menu
+function menuToggle() {
+  const menuToggle = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
